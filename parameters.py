ba417f405410e3de8a121be642830e2658c30cf2
yuyao <yuyao_wang@foxmail.com>
2024-09-06 16:21:20 -0600

    parameter

--- parameters.py
+++ parameters.py
@@ -13,7 +13,7 @@ def generate_parameters(
     D = 0.96343 * 7,  # cm 
     # D = 1.02,  
     # sigma_a =0.0835   
-    sigma_a=1.58430*1e-2/7.4, # cm^-1        
+    sigma_a=1.58430*1e-2/7.35, # cm^-1   7.4-explode, 7.3-converge to 0     
     nu_sigma_f = 3e-2/6.8, # cm^-1, 3.33029e-2
     # sigma_a = 0.00054869,
     # nu_sigma_f = 0.00098328,
