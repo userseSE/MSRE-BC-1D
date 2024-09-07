9b4c8d90b485ab3848e1543830a711532b1f565d
yuyao <yuyao_wang@foxmail.com>
2024-09-07 11:21:58 -0600

    parameter

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from transport_delay import transport_delay
 from power_plant import power_plant_temp
 
 def run_simulation(params, index):
-    time_span = 10000
+    time_span = 20000
     N = params['N']
     Nx = params['Nx']
 
@@ -228,7 +228,7 @@ def main():
     # Define ranges of values for parameters
     # V_values = np.linspace(1.103497e6, 1.103497e8, 5)
     # D_values = np.linspace(0.96343*7, 0.96343*8, 5)     
-    sigma_a_values=np.linspace(0.00216187, 0.00216195, 25) # cm^-1        
+    sigma_a_values=np.linspace(0.002161937, 0.002161940, 30) # cm^-1        
     # nu_sigma_f_values = np.linspace(3.33029e-2/7, 3.33029e-2/8, 5) # cm^-1
     # L=22.9
 
