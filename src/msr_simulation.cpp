b711cd9a8961e51c25be8572e0444db472d3cc29
yuyao <yuyao_wang@foxmail.com>
2024-11-04 11:19:55 -0700

    modify main to top function

--- src/msr_simulation.cpp
+++ src/msr_simulation.cpp
@@ -4,10 +4,9 @@
 #include "parameters.hpp"
 #include "reactivity.hpp"
 #include "thermal_hydraulics.hpp"
+// #include <iostream>
 
-#include <iostream>
-
-int main() {
+void msr_simulation() {
   Param_Neutronics params_neutr;
   // Initialization
   float rho[N];
@@ -17,12 +16,11 @@ int main() {
   float y_hx2[length_hx];
 
   for (int step = 0; step < time_span; ++step) {
-    std::cout << "Time step: " << step << std::endl;
+    // std::cout << "Time step: " << step << std::endl;
     neutronics(y_n, rho, step, params_neutr);
     thermal_hydraulics(y_th, y_n,y_hx1[0], step);
     HX1(y_hx1, y_th[N - 1], step);
     HX2(y_hx2, y_hx1[Nx - 1], step);
     reactivity(y_th, y_th + N, step, rho);
   }
-  return 0;
 }
